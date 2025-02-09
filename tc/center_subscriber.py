e13b82d8f31c705b021b3f9cfb779165fead52aa
wyseo10 <realmeteny10@gmail.com>
2025-02-10 00:35:57 +0900

    Using tele_key is available

--- tc/center_subscriber.py
+++ tc/center_subscriber.py
@@ -1,20 +1,147 @@
 import rclpy
+import math
+import numpy as np
 from rclpy.node import Node
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import Point, Twist
+from crazyflie_py.crazyflie import CrazyflieServer, TimeHelper
 
 class CenterSubscriber(Node):
     def __init__ (self):
         super().__init__('center_subscriber')
-        self.subscription = self.create_subscription(
+        self.subscription_box = self.create_subscription(
             Point,
             'max_box_center',
-            self.listener_callback,
+            self.listener_box_callback,
             10
         )
-        self.subscription
+        
+        self.subscription_cmdvel = self.create_subscription(
+            Twist,
+            'cmd_vel',
+            self.listener_cmdvel_callback,
+            10
+        )
+
+        timer_period = 0.01  # 최대 100Hz
+        self.timer = self.create_timer(timer_period, self.timer_control_callback)
+
+        self.get_logger().info('Wait for server')
+        self.Z = 0.8
+        self.allcfs = CrazyflieServer()
+        self.timeHelper = TimeHelper(self.allcfs)
+
+        self.flag_box_msg = False
+        self.flag_takeoff = False
+        self.flag_land = False
+        self.flag_ccw = False
+        self.flag_cw = False
+        self.flag_front = False
+        self.flag_back = False
+        self.flag_left = False
+        self.flag_right = False
+
+        self.get_logger().info('Sub ready')
+
+
+    def takeoff(self):    
+        self.allcfs.takeoff(targetHeight=self.Z, duration=1.0 + self.Z)
+        self.timeHelper.sleep(1.5 + self.Z)
+        self.get_logger().info('Take off')
+        self.flag_takeoff = False
+        
+    def land(self):
+        self.allcfs.land(targetHeight=0.04, duration=2.5)
+        self.timeHelper.sleep(1.0+self.Z)
+        self.get_logger().info('Land')
+        self.flag_land = False
+
+    def ccw(self):
+        for cf in self.allcfs.crazyflies:
+            cf.goTo(np.array([0.001, 0, 0]), math.pi / 8.0, 0.05, relative=True)
+        self.get_logger().info('CCW')
+        self.flag_ccw = False
+
+    def cw(self):
+        for cf in self.allcfs.crazyflies:
+            cf.goTo(np.array([0.001, 0, 0]), -math.pi / 8.0, 0.05, relative=True)
+        self.get_logger().info('CW')
+        self.flag_cw = False
+
+    def front(self):
+        for cf in self.allcfs.crazyflies:
+            cf.goTo(np.array([0.2, 0, 0]), 0, 0.05, relative=True)
+        self.get_logger().info('Front')
+        self.flag_front = False
+
+    def back(self):
+        for cf in self.allcfs.crazyflies:
+            cf.goTo(np.array([-0.2, 0, 0]), 0, 0.05, relative=True)
+        self.get_logger().info('Back')
+        self.flag_back = False
+
+    def left(self):
+        for cf in self.allcfs.crazyflies:
+            cf.goTo(np.array([0, 0.2, 0]), 0, 0.05, relative=True)
+        self.get_logger().info('Left')
+        self.flag_left = False
     
-    def listener_callback(self, msg):
+    def right(self):
+        for cf in self.allcfs.crazyflies:
+            cf.goTo(np.array([0, -0.2, 0]), 0, 0.05, relative=True)
+        self.get_logger().info('Right')
+        self.flag_right = False
+
+
+    def timer_control_callback(self):
+        if self.flag_takeoff and self.flag_box_msg:
+            self.takeoff()
+
+        if self.flag_land:
+            self.land()
+
+        if self.flag_ccw:
+            self.ccw()
+            
+        if self.flag_cw:
+            self.cw()
+
+        if self.flag_front:
+            self.front()
+        
+        if self.flag_back:
+            self.back()
+
+        if self.flag_left:
+            self.left()
+
+        if self.flag_right:
+            self.right()
+
+    def listener_box_callback(self, msg):        
         self.get_logger().info(f'Subscribing : x={msg.x:.2f}, y={msg.y:.2f}')
+        self.flag_box_msg = True
+
+    def listener_cmdvel_callback(self, msg):
+        self.get_logger().info(f'Cmd_vel : line_x = {msg.linear.x:.2f}, ang_z = {msg.angular.z:.2f}')
+        
+        if msg.linear.x == 0.5 and msg.angular.z == 1.0: # "u" is pressed
+            self.flag_ccw = True
+        if msg.linear.x == 0.5 and msg.angular.z == 0.0: # "i" is pressed
+            self.flag_front = True
+        if msg.linear.x == 0.5 and msg.angular.z == -1.0: # "o" is pressed
+            self.flag_cw = True
+        if msg.linear.x == 0.0 and msg.angular.z == 1.0: # "j" is pressed
+            self.flag_left = True
+        if msg.linear.x == 0.0 and msg.angular.z == 0.0: # "k" is pressed
+            self.flag_back = True
+        if msg.linear.x == 0.0 and msg.angular.z == -1.0: # "l" is pressed
+            self.flag_right = True
+        #if msg.linear.x == -0.5 and msg.angular.z == -1.0: # "m" is pressed
+        
+        if msg.linear.x == -0.5 and msg.angular.z == 0.0: # "," is pressed
+            self.flag_takeoff = True
+        if msg.linear.x == -0.5 and msg.angular.z == 1.0: # "." is pressed
+            self.flag_land = True
 
 def main(args=None):
     rclpy.init(args=args)
